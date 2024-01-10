11240ace7819b7354ec4153614020a61cad750f1
JAldo1980 <jamesalderman1980@gmail.com>
2024-01-10 09:34:39 +0000

    added files

---
+++ index.js
@@ -0,0 +1,52 @@
+const genBtn = document.getElementById("gen-btn");
+
+genBtn.addEventListener("click", createPassword);
+
+const randomCharacters = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "!",
+  "@",
+  "£",
+  "$",
+  "%",
+  "^",
+  "&",
+  "?",
+  "+",
+  "J",
+  "P",
+  "M",
+  "D",
+  "R",
+  "S",
+  "A",
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+];
+
+const generatedPassword = [];
+
+function createPassword() {
+  generatedPassword.length = 0;
+
+  let inputValue = document.getElementById("number-input").value;
+  for (let i = 0; i < inputValue; i++) {
+    let randomNumber = Math.floor(Math.random() * randomCharacters.length);
+
+    let randomPassword = randomCharacters[randomNumber];
+    generatedPassword.push(randomPassword);
+  }
+  console.log(generatedPassword);
+}
